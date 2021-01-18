77f504a488850516fa9b9a3320d58ab3e469adf5
grilla001 <a.grill@student.reading.ac.uk>
2021-01-18 18:07:01 +0000

    Actor practice

--- build.sbt
+++ build.sbt
@@ -10,6 +10,5 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
-  "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
   "org.scalatest" %% "scalatest" % "3.1.0" % Test
 )
---
+++ src/main/scala/com/example/ActorHierarachyExperiments.scala
@@ -0,0 +1,83 @@
+package com.example
+
+import akka.actor.typed.{ActorSystem, Behavior, PostStop, Signal}
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+
+object PrintMyActorRefActor {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new PrintMyActorRefActor(context))
+}
+
+class PrintMyActorRefActor(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "printit" =>
+        val secondRef = context.spawn(Behaviors.empty[String], "second-actor")
+        println(s"Second: $secondRef")
+        this
+    }
+}
+
+object Main {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new Main(context))
+}
+
+class Main(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "start" =>
+        val firstRef = context.spawn(PrintMyActorRefActor(), "first-actor")
+        println(s"First: $firstRef")
+        firstRef ! "printit"
+        this
+    }
+}
+
+object StartStopActor1 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(context => new StartStopActor1(context))
+}
+
+class StartStopActor1(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("first started")
+  context.spawn(StartStopActor2(), "second")
+
+  override def onMessage(msg: String): Behavior[String] =
+    msg match {
+      case "stop" => Behaviors.stopped
+    }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("first stopped")
+      this
+  }
+}
+
+object StartStopActor2 {
+  def apply(): Behavior[String] =
+    Behaviors.setup(new StartStopActor2(_))
+}
+
+class StartStopActor2(context: ActorContext[String]) extends AbstractBehavior[String](context) {
+  println("second started")
+
+  override def onMessage(msg: String): Behavior[String] = {
+    Behaviors.unhandled
+  }
+
+  override def onSignal: PartialFunction[Signal, Behavior[String]] = {
+    case PostStop =>
+      println("second stopped")
+      this
+  }
+}
+
+object ActorHierarachyExperiments extends App {
+  val testSystem = ActorSystem(Main(), "testSystem")
+  testSystem ! "start"
+}
