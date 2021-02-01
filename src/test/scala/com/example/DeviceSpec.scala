cdeb144fafa4a2997ecad7ab61251e2d404db8c6
grilla001 <a.grill@student.reading.ac.uk>
2021-02-01 14:16:10 +0000

    Add test case

---
+++ src/test/scala/com/example/DeviceSpec.scala
@@ -0,0 +1,19 @@
+import akka.actor.testkit.typed.scaladsl.ScalaWithActorTestKit
+import com.example.Device.RespondTemperature
+import org.scalatest.wordspec.AnyWordSpecLike
+
+class DeviceSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+  import Device._
+
+  "Device actor" must {
+    "reply with empty reading if no tempertature is known" in {
+      val probe = createTestProbe[RespondTemperature]()
+      val deviceActor = spawn(Device("group", "device"))
+
+      deviceActor ! Device.ReadTemperature(requestId = 42, probe.ref)
+      val response = probe.receiveMessage()
+      response.requestId should ===(42)
+      response.value should ===(None)
+    }
+  }
+}
